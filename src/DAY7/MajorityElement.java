08b8b7fadd5ee100f3a530a4c629b49c88c5b95d
suryansh123-cyber <suri.vashistha@gmail.com>
2026-02-07 22:43:09 +0530

    DAY7 Added

---
+++ src/DAY7/MajorityElement.java
@@ -0,0 +1,16 @@
+public class MajorityElement {
+    static int majority(int[] nums) {
+        int count = 0, candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) candidate = num;
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,2,1,1,1,2,2};
+        System.out.println(majority(arr));
+    }
+}
