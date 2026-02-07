d19df70b7e9484e315b1131f4f7d15b16bd8a6f0
suryansh123-cyber <suri.vashistha@gmail.com>
2026-02-07 23:09:19 +0530

    More practised questions added

---
+++ src/DAY7/FindDuplicate.java
@@ -0,0 +1,22 @@
+public class FindDuplicate {
+    static int findDuplicate(int[] nums) {
+        int slow = nums[0], fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,2,2};
+        System.out.println(findDuplicate(arr));
+    }
+}
