c62710e903334591e927da8b4b905522248b2fbb
suryansh123-cyber <suri.vashistha@gmail.com>
2026-02-17 17:59:18 +0530

    question practised

---
+++ src/DAY17/func.java
@@ -0,0 +1,11 @@
+public class func {
+    
+    public static int add(int a, int b){
+        return a + b;
+    }
+    
+    public static void main(String[] args) {
+        int sum = add(5,10);
+        System.out.println(sum);
+    }    
+}
